656007f1644559b7ed127b696e4b5f5ff930cf9d
amy861113 <amy86881113amy123@gmail.com>
2020-05-19 23:47:08 +0800

    [edit]animal_20200519

--- animal/animal.py
+++ animal/animal.py
@@ -16,7 +16,7 @@ def read_data(db):
         if os.path.isdir(db+files+'/'):
             for img in os.listdir(db+files+'/'):
                 X.append(db+files+'/'+img)
-                Y.append(int(files)-1)
+                Y.append(int(files))
     return X, Y
 
 trans = Transformer()
@@ -24,47 +24,23 @@ trans.add(lambda img: cv2.resize(img, (224, 224)))
 trans.add(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 
-
-'''def resize(imgs, size):
-    resize_imgs = []
-    for img in imgs:
-        resize_imgs.append(cv2.resize(img, size))
-    return np.array(resize_imgs)'''
-
 DB = 'animal_train_data/'
 imgs, labels = read_data(DB)
-
 labels = one_hot(labels, 4)
 dataset = ImageDataset(imgs, labels, transformer=trans)
-gen = MyGenerator(dataset, 10)
-
-'''imgs, labels = dataset[0:3]
-print(imgs.shape)
-print(labels)'''
-
-'''print(len(trainDataset))
-print(trainDataset[3])'''
-
-
-'''for ims, lbs in gen:
-    im = ims[0]
-    
-    plt.imshow(im)
-    plt.show()'''
-
-'''print(imgs)
-print(labels)
-print(os.listdir(DB))'''
 
 from tensorflow.keras import metrics
 
-net = myModel(output=4, input_shape=(224, 224, 3))
+net = vgg16(output=4, input_shape=(224, 224, 3))
 net.summary()
 
-
-batcher = Batcher(dataset, 10, shuffle=True)
+batcher = Batcher(dataset, 20, shuffle=True)
 net.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=[metrics.categorical_accuracy])
-net.fit(x=batcher, epochs=10)
+net.fit(x=batcher, epochs=100)
+
+net.save('animal.h5')
+
+
 
 
 
---
+++ animal/predict.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+
+tmp = []
+testDB = 'animal_test_data/'
+test_imgs = testDB+'haung.jpg'
+
+#test_labels = one_hot(test_labels, 4)
+test_img = cv2.imread(test_imgs)
+test_img = cv2.resize(test_img, (224, 224))
+test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+tmp.append(test_img)
+test = np.array(tmp)
+
+net = load_model('animal.h5')
+predict = net.predict(test)
+print(predict)
+#print(test_labels[0])
+'''count = (predict.argmax(axis=1) == test_labels[0].argmax(axis=1)).sum()
+print(count/len(test))'''
